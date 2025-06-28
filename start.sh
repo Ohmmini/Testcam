2c2a03da00a780cbec37562008b4d798be361513
Ohm Nippish <nippit63@gmail.com>
2025-06-28 11:47:57 +0700

    test2 render

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-chmod +x ./cloudflared
+
+# ✅ ดาวน์โหลด cloudflared
+wget -O cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+
+# ✅ เปลี่ยน permission
+chmod +x cloudflared
+
+# ✅ สร้าง config.yml แบบใช้ environment variables
+echo "tunnel: $TUNNEL_ID
+credentials-file: $CREDENTIALS_FILE
+ingress:
+  - hostname: $HOSTNAME
+    service: kwangdataisyourspace.space
+  - service: http_status:404" > config.yml
+
+# ✅ รัน tunnel
 ./cloudflared tunnel --config config.yml run
